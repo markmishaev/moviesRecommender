60de73efd93a2aad2773b3ba67fd48385204ace9
markmishaev <mark.mishaev@gmail.com>
2015-04-20 17:25:18 +0300

    User Authentication Added

--- ngMovieStub-master/index.js
+++ ngMovieStub-master/index.js
@@ -4,9 +4,15 @@ var request = require("request")
 var Movie = require('./public/js/models/movie');
 var MoviesData = require('./data/moviesData');
 var file1 = './data/generatedMovies1.json'
-	
+var http = require('http');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;	
 var express = require('express')
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var methodOverride = require('method-override')
+var session = require('express-session')
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/movies');
@@ -24,19 +30,81 @@ db.once('open', function ()
 
 
 var app = express()
-var bookings = [];
 
+//MoviesData.prepareMovieData(0);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-//MoviesData.prepareMovieData(0);
+app.use(cookieParser()); 
+app.use(methodOverride());
+app.use(session({
+    secret: 'securedsession',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
 
 		
 app.set("view options", {
     layout: false
 });
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
+
+//=========================Users=========================================
+
+//Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+		  function(username, password, done) {
+		    if (username === "admin" && password === "admin") // stupid example
+		      return done(null, {name: "admin"});
+
+		    return done(null, false, { message: 'Incorrect username.' });
+		  }
+		));
+
+		//Serialized and deserialized methods when got from session
+		passport.serializeUser(function(user, done) {
+		    done(null, user);
+		});
+
+		passport.deserializeUser(function(user, done) {
+		    done(null, user);
+		});
+
+		// Define a middleware function to be used for every secured routes
+		var auth = function(req, res, next){
+		  if (!req.isAuthenticated()) 
+		  	res.send(401);
+		  else
+		  	next();
+		};
+
+
+app.get('/users', auth, function(req, res){
+	  res.send([{name: "user1"}, {name: "user2"}]);
+	});
+
+//route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+//route to log in
+app.post('/login', passport.authenticate('local'), function(req, res) {
+res.send(req.user);
+});
+
+//route to log out
+app.post('/logout', function(req, res){
+req.logOut();
+res.send(200);
+});
+
+//=========================Movies=========================================
 
 app.get('/', function (req, res) {
     res.render('public/index.html');
@@ -46,23 +114,6 @@ app.get('/movies', function (req, res)
 {
     var movies1 = require(MoviesData.getOutputFileName());	
     res.json(movies1);
-});
-
-
-app.get('/bookings', function (req, res) {
-    res.json(bookings);
-});
-
-app.post('/book', function (req, res) {
-    var data = {
-        'qty': req.body.qty,
-        'date': req.body.date,
-        'id': req.body.movie_id,
-        'name': req.body.movie_name
-    };
-    bookings.push(data);
-    // res.render('public/tmpl/bookings.html');
-    res.json(bookings);
 });
 
 
